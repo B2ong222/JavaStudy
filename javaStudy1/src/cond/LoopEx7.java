552b26cf7c94d2f8ac962718cbe6e27836974098
bong <bongchok45@gmail.com>
2026-03-15 20:10:57 +0900

    반복문 예제6

---
+++ javaStudy1/src/cond/LoopEx7.java
@@ -0,0 +1,12 @@
+package cond;
+
+public class LoopEx7 {
+    static void main() {
+        for (int i = 1; i <= 5; i++){
+            for (int j = 1; j <= 5; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
