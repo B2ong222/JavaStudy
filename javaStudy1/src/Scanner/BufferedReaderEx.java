53d8967d937e331e9e386ee8011485d900239231
bong <bongchok45@gmail.com>
2026-03-18 16:59:32 +0900

    스캐너와 유사 BufferedReader

---
+++ javaStudy1/src/Scanner/BufferedReaderEx.java
@@ -0,0 +1,22 @@
+package Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReaderEx {
+    static void main() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("문자열을 입력하세요: ");
+        String str = br.readLine();
+        System.out.println("입력한 문자열: " + str);
+
+        System.out.print("정수를 입력하세요: ");
+        int intValue = Integer.parseInt(br.readLine());
+        System.out.println("입력한 문자열: " + intValue);
+
+        System.out.print("실수를 입력하세요: ");
+        double doubleValue = Double.parseDouble(br.readLine());
+        System.out.println("입력한 문자열: " + doubleValue);
+    }
+}
