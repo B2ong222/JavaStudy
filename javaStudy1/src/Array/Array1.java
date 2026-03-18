ead3f939cb84f11de597c104a28d8a816780602f
bong <bongchok45@gmail.com>
2026-03-18 18:06:38 +0900

    배열

---
+++ javaStudy1/src/Array/Array1.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class Array1 {
+    static void main() {
+        int[] students;
+        students = new int[5];
+        students[0] = 1;
+        students[1] = 2;
+        students[2] = 3;
+        students[3] = 4;
+        students[4] = 5;
+        System.out.println("학1: "+students[0]);
+        System.out.println("학2: "+students[1]);
+        System.out.println("학3: "+students[2]);
+        System.out.println("학4: "+students[3]);
+        System.out.println("학5: "+students[4]);
+
+    }
+}
