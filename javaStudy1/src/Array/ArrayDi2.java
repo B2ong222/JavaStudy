065073c941c8a5fa8de433f3008209c5fa34d204
bong <bongchok45@gmail.com>
2026-03-18 18:08:47 +0900

    2중 배열2

---
+++ javaStudy1/src/Array/ArrayDi2.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class ArrayDi2 {
+    static void main() {
+        int[][] arr = new int[2][3]; // 행(row), 열(column)
+
+        arr[0][0] = 1; // 0행, 0열
+        arr[0][1] = 2; // 0행, 1열
+        arr[0][2] = 3; // 0행, 2열
+        arr[1][0] = 4; // 1행, 0열
+        arr[1][1] = 5; // 1행, 1열
+        arr[1][2] = 6; // 1행, 2열
+
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
