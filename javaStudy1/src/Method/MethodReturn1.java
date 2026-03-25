daa1893922da45ffa01ca381ed2c15e934f630d2
bong <bongchok45@gmail.com>
2026-03-25 23:44:37 +0900

    메소드 리턴 1

---
+++ javaStudy1/src/Method/MethodReturn1.java
@@ -0,0 +1,14 @@
+//package Method;
+//
+//public class MethodReturn1 {
+//    static void main() {
+//        boolean result = odd(2);
+//        System.out.println(result);
+//    }
+//
+//    static boolean odd(int i) {
+//        if (i%2==1){
+//            return true;
+//        }
+//    }
+//}
