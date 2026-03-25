e7a0d144204bf1638d172d4d4e4e0c46507c6224
bong <bongchok45@gmail.com>
2026-03-25 23:38:24 +0900

    형성평가 4

---
+++ javaStudy1/src/formativeEvaluation/four.java
@@ -0,0 +1,13 @@
+package formativeEvaluation;
+
+public class four {
+    static void main() {
+        for (int i = 1; i <= 20; i++){
+            if (i%2==1){
+                System.out.print(i + " ");
+            } else {
+                continue;
+            }
+        }
+    }
+}
