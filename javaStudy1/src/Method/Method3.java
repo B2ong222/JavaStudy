70325082dc51bee1ad7c1ba4b9b2292c999e5bbd
bong <bongchok45@gmail.com>
2026-03-25 23:44:04 +0900

    메소드 3

---
+++ javaStudy1/src/Method/Method3.java
@@ -0,0 +1,15 @@
+package Method;
+
+public class Method3 {
+    static void main() {
+        printHeader();
+        System.out.println("프로그램 동작");
+        printFooter();
+    }
+    public static void printHeader() {
+        System.out.println("프로그램 시작");
+    }
+    public static void printFooter() {
+        System.out.println("프로그램 시작");
+    }
+}
