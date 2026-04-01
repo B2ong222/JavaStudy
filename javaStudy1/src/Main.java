b5aa6709fc75d9f6170069dac790d315159ee2b1
bong <bongchok45@gmail.com>
2026-04-01 20:47:11 +0900

    .

--- javaStudy1/src/Main.java
+++ javaStudy1/src/Main.java
@@ -1,8 +1,22 @@
 import Class1.Book;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Book b = new Book("흰", "한강", 15000);
-        System.out.println("도서명: " +  b.title + ", 작가: " +b.author + ", 가격: "+ b.price +"원");
+        Scanner sc = new Scanner(System.in);
+
+        // 사용자로부터 입력 받기
+        System.out.print("책 제목을 입력하세요: ");
+        String title = sc.nextLine();
+
+        System.out.print("저자를 입력하세요: ");
+        String author = sc.nextLine();
+
+        System.out.print("가격을 입력하세요: ");
+        int price = sc.nextInt();
+
+        // 입력받은 값으로 객체 생성
+        Book b = new Book(title, author, price);
+        b.info();
     }
 }
