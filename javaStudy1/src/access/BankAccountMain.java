d8ac51015dce52fd36a44e91f8b9639fb05b98ef
bong <bongchok45@gmail.com>
2026-04-07 23:48:49 +0900

    은행은행은행

---
+++ javaStudy1/src/access/BankAccountMain.java
@@ -0,0 +1,13 @@
+package access;
+
+public class BankAccountMain {
+    static void main() {
+        BankAccount account = new BankAccount();
+
+        account.deposit(10000);
+
+        account.withdraw(3000);
+
+        System.out.println("balance = " + account.getBalance());
+    }
+}
