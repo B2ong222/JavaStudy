dfef815c47b7dac5f2a6245006048a8acbf4c331
bong <bongchok45@gmail.com>
2026-04-08 09:08:23 +0900

    accessEx 1

--- javaStudy1/src/access/ex/AccessEx1.java
+++ javaStudy1/src/access/ex/AccessEx1.java
@@ -1,4 +1,30 @@
 package access.ex;
 
 public class AccessEx1 {
+    static void main() {
+        MaxCounter counter = new MaxCounter(3);
+        counter.increment();
+        counter.increment();
+        counter.increment();
+        counter.increment();
+        int count = counter.getCount();
+        System.out.println(count);
+    }
+}
+
+class MaxCounter {
+    private int count;
+    private int max;
+    public MaxCounter(int max) {
+        this.count = 0;
+        this.max = max;
+    }
+    public void increment() {
+        if (count >= max) {
+            System.out.println("최대값초과x");
+            return;
+        }
+        count++;
+    }
+    public int getCount() { return count; }
 }
