a7683f5c3fa2405d72501b116a2cf951e467b2c3
bong <bongchok45@gmail.com>
2026-04-15 23:15:22 +0900

    extends2

---
+++ javaStudy1/src/extends2/ElectricCar.java
@@ -0,0 +1,11 @@
+package extends2;
+
+public class ElectricCar extends Car {
+    public void charge(){
+        System.out.println("충전");
+    }
+
+    public void openDoor() {
+
+    }
+}
---
+++ javaStudy1/src/extends2/GasCar.java
@@ -0,0 +1,15 @@
+package extends2;
+
+public class GasCar extends Car {
+    public void fillup(){
+        System.out.println("기름충전");
+    }
+
+    public void openDoor() {
+
+    }
+
+    public void fillUp() {
+
+    }
+}
