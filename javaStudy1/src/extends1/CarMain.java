90d825589648e859460959155f1e10f4e6387bc8
bong <bongchok45@gmail.com>
2026-04-15 23:15:00 +0900

    extends1

---
+++ javaStudy1/src/extends1/CarMain.java
@@ -0,0 +1,12 @@
+package extends1;
+
+public class CarMain {
+    static void main() {
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.move();
+        electricCar.charge();
+        GasCar gasCar = new GasCar();
+        gasCar.move();
+        gasCar.fillup();
+    }
+}
---
+++ javaStudy1/src/extends1/GasCar.java
@@ -0,0 +1,10 @@
+package extends1;
+
+public class GasCar {
+    public void move() {
+        System.out.println("자동차 이동");
+    }
+    public void fillup(){
+        System.out.println("기름충전");
+    }
+}
