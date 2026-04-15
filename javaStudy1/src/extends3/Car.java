414541b8bc09ea7e8705ecf5ec39b0744cb9bb00
bong <bongchok45@gmail.com>
2026-04-15 23:15:35 +0900

    extends3

---
+++ javaStudy1/src/extends3/Car.java
@@ -0,0 +1,10 @@
+package extends3;
+
+public class Car {
+    public void move() {
+        System.out.println("차 이동");
+    }
+    public void openDoor() {
+        System.out.println("문염");
+    }
+}
---
+++ javaStudy1/src/extends3/CarMain.java
@@ -0,0 +1,23 @@
+package extends3;
+
+import extends2.ElectricCar;
+import extends2.GasCar;
+
+public class CarMain{
+    static void main() {
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.move();
+        electricCar.charge();
+        electricCar.openDoor();
+
+        GasCar gasCar = new GasCar();
+        gasCar.move();
+        gasCar.fillUp();
+        gasCar.openDoor();
+
+        HydrogenCar hydrogenCar = new HydrogenCar();
+        hydrogenCar.move();
+        hydrogenCar.fillHydrogen();
+        hydrogenCar.openDoor();
+    }
+}
